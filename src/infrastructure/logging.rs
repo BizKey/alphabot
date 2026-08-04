3ff27877707a3327a7d4f6fbafd895a6a4d76c09
kizianov.ao <kizianov.ao@svrauto.ru>
2026-08-04 09:11:13 +0000

    .

---
+++ src/infrastructure/logging.rs
@@ -0,0 +1,7 @@
+pub fn init_tracing() {
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_target(true)
+        .with_thread_ids(true)
+        .init();
+}
